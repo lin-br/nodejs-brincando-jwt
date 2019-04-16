7bdfed05bd3cf9fc537086dd2e134ee273b6459e
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-16 15:09:07 -0300

    Implementar funcionalidade para verificar token JWT

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+const chave = 'txp2wk99k6UKGM9Lr+DX29v0lntp9RTwQUiv+ZzOYoA=';
+
 app.post('/login', function (request, response) {
     let {usuario, senha} = request.body;
 
@@ -12,12 +14,37 @@ app.post('/login', function (request, response) {
 
     console.log(usuario, senha, payload);
 
-    let token = apiJwt.sign(payload, 'txp2wk99k6UKGM9Lr+DX29v0lntp9RTwQUiv+ZzOYoA=', {
+    let token = apiJwt.sign(payload, chave, {
         algorithm: 'HS512',
-        expiresIn: 10,
+        expiresIn: 300,
     });
 
     response.send(token);
+});
+
+app.get('/', function (request, response) {
+    let headerAuthorization = request.headers.authorization;
+    let headerXAccessToken = request.header('x-access-token');
+
+    if (headerAuthorization || headerAuthorization) {
+
+        let token = headerAuthorization ? headerAuthorization.replace('Bearer ', '') : headerXAccessToken;
+
+        console.log(token);
+
+        try {
+            let verify = apiJwt.verify(token, chave);
+
+            console.log(verify);
+
+            response.send('ok');
+        } catch (e) {
+            console.log(e);
+            response.status(401).send();
+        }
+    } else {
+        response.status(401).send();
+    }
 });
 
 app.listen('3000', function () {
