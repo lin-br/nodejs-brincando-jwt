879eac2e43574e3c93f69b4c42abbbcea0e1bd9b
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-16 12:34:13 -0300

    Implementar rota login com geração de um token via JWT

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const apiJwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/login', function (request, response) {
-    response.send('OK');
+    let {usuario, senha} = request.body;
+
+    let payload = usuario === 'teste' && senha === '1234' ? {'id': 32141} : null;
+
+    console.log(usuario, senha, payload);
+
+    let token = apiJwt.sign(payload, 'txp2wk99k6UKGM9Lr+DX29v0lntp9RTwQUiv+ZzOYoA=', {
+        algorithm: 'HS512',
+        expiresIn: 10,
+    });
+
+    response.send(token);
 });
 
 app.listen('3000', function () {
